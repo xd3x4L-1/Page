8c42679cdd124862548467ad2cf4faa979fac5c3
xd3x4L-1 <bochrister.1.4@telia.com>
2015-12-15 10:18:31 +0100

    Update CForm.php

--- page/trial/src/CForm/CForm.php
+++ page/trial/src/CForm/CForm.php
@@ -153,7 +153,7 @@ class CFormElementTextarea extends CFormElement {
 */
   public function __construct($name, $attributes=array()) {
     parent::__construct($name, $attributes);
-    //$this['type'] = 'textarea';
+    $this['type'] = 'textarea';
     $this->UseNameAsDefaultLabel();
   }
 }
